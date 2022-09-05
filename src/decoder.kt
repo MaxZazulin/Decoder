0ac4407a323aa0776fd93c6581fdd52a4d3587b4
MaximZazulin <88067751+MaximZazulin@users.noreply.github.com>
2022-09-06 01:59:14 +0500

    :(

--- src/decoder.kt
+++ src/decoder.kt
@@ -2,9 +2,19 @@ import java.io.File
 import java.util.*
 
 fun main() {
-    val scan = Scanner(System.`in`)
-    val n : String = scan.nextLine()
-    decoderFile(n)
+    val shift : Int = 3
+    val test : Array<Char> = arrayOf('m','a','x')
+    test.forEach { name -> print(name)}
+    println()
+    val alpha : Array<Char> = arrayOf(
+        'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m', 'n','o','p','q','r','s','t','u','v','w','x','y','z')
+    for (i in test.indices)
+        for (j in alpha.indices) {
+            if (test[i] == alpha[j])
+                test[i] = alpha[j + shift]
+            break
+        }
+    test.forEach { name2 -> print(name2)}
 }
 //fun readFile(name: String) {
 //    val file = File(/* pathname = */ "txt.txt") // чтение файла
@@ -14,6 +24,6 @@ fun main() {
 //        println(line)
 //    }
 //}
-fun decoderFile(name: String) {
-
-}
+//    val scan = Scanner(System.`in`)
+//    val n : String = scan.nextLine()
+//    decoderFile(n)
