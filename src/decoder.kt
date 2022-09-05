dde42fd3f2e9b1211a06a8709d505321b642f80e
MaximZazulin <88067751+MaximZazulin@users.noreply.github.com>
2022-09-05 06:44:47 +0500

    :(

---
+++ src/decoder.kt
@@ -0,0 +1,19 @@
+import java.io.File
+import java.util.*
+
+fun main() {
+    val scan = Scanner(System.`in`)
+    val n : String = scan.nextLine()
+    decoderFile(n)
+}
+//fun readFile(name: String) {
+//    val file = File(/* pathname = */ "txt.txt") // чтение файла
+//    val bufferedReader = file.bufferedReader()
+//    val text = bufferedReader.readLines()
+//    for(line in text){
+//        println(line)
+//    }
+//}
+fun decoderFile(name: String) {
+
+}
